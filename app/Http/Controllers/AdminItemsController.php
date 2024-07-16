770e82d3a810a4873b5cc05b2c39d679568582d7
Mike Rodelas <mikerodelas@digits.ph>
2024-07-16 12:30:54 +0800

    add function to item module for syncing on-hand qty for retail

--- app/Http/Controllers/AdminItemsController.php
+++ app/Http/Controllers/AdminItemsController.php
@@ -1,10 +1,14 @@
-<?php namespace App\Http\Controllers;
+<?php 
+
+	namespace App\Http\Controllers;
 
 	use Session;
 	use Request;
 	use DB;
 	use CRUDBooster;
 	use App\Http\Controllers\POSPushController;
+	use App\Item;
+	use App\OnhandQty;
 
 	class AdminItemsController extends \crocodicstudio\crudbooster\controllers\CBController {
 
@@ -69,47 +73,7 @@
 			foreach($distriSubs as $dSub){
 			    $this->form[] = ["label"=>ucwords(strtolower($dSub->subinventory))." Reserve Qty","name"=>strtolower(str_replace(" ","_", $dSub->subinventory))."_reserve_qty",'type'=>'number','validation'=>'required|integer|min:0','width'=>'col-sm-4'];
 			}
-			# END FORM DO NOT REMOVE THIS LINE
-
-			/* 
-	        | ---------------------------------------------------------------------- 
-	        | Sub Module
-	        | ----------------------------------------------------------------------     
-			| @label          = Label of action 
-			| @path           = Path of sub module
-			| @foreign_key 	  = foreign key of sub table/module
-			| @button_color   = Bootstrap Class (primary,success,warning,danger)
-			| @button_icon    = Font Awesome Class  
-			| @parent_columns = Sparate with comma, e.g : name,created_at
-	        | 
-	        */
-	        $this->sub_module = array();
-
-
-	        /* 
-	        | ---------------------------------------------------------------------- 
-	        | Add More Action Button / Menu
-	        | ----------------------------------------------------------------------     
-	        | @label       = Label of action 
-	        | @url         = Target URL, you can use field alias. e.g : [id], [name], [title], etc
-	        | @icon        = Font awesome class icon. e.g : fa fa-bars
-	        | @color 	   = Default is primary. (primary, warning, succecss, info)     
-	        | @showIf 	   = If condition when action show. Use field alias. e.g : [id] == 1
-	        | 
-	        */
-	        $this->addaction = array();
-
-
-	        /* 
-	        | ---------------------------------------------------------------------- 
-	        | Add More Button Selected
-	        | ----------------------------------------------------------------------     
-	        | @label       = Label of action 
-	        | @icon 	   = Icon from fontawesome
-	        | @name 	   = Name of button 
-	        | Then about the action, you should code at actionButtonSelected method 
-	        | 
-	        */
+			
 	        $this->button_selected = array();
             if(CRUDBooster::isSuperadmin())
 	        {
@@ -124,149 +88,29 @@
 				$this->button_selected[] = ['label'=>'Create POS Item',
 											'icon'=>'fa fa-check-circle-o',
 											'name'=>'create_pos_item'];
+
+				$this->button_selected[] = ['label'=>'Sync Item RTL Qty',
+											'icon'=>'fa fa-check-circle-o',
+											'name'=>'sync_rtl_qty'];
 	        }
-	                
-	        /* 
-	        | ---------------------------------------------------------------------- 
-	        | Add alert message to this module at overheader
-	        | ----------------------------------------------------------------------     
-	        | @message = Text of message 
-	        | @type    = warning,success,danger,info        
-	        | 
-	        */
-	        $this->alert = array();
-	                
 
-	        
-	        /* 
-	        | ---------------------------------------------------------------------- 
-	        | Add more button to header button 
-	        | ----------------------------------------------------------------------     
-	        | @label = Name of button 
-	        | @url   = URL Target
-	        | @icon  = Icon from Awesome.
-	        | 
-	        */
-	        $this->index_button = array();
-
-
-
-	        /* 
-	        | ---------------------------------------------------------------------- 
-	        | Customize Table Row Color
-	        | ----------------------------------------------------------------------     
-	        | @condition = If condition. You may use field alias. E.g : [id] == 1
-	        | @color = Default is none. You can use bootstrap success,info,warning,danger,primary.        
-	        | 
-	        */
-	        $this->table_row_color = array();     	          
-
-	        
-	        /*
-	        | ---------------------------------------------------------------------- 
-	        | You may use this bellow array to add statistic at dashboard 
-	        | ---------------------------------------------------------------------- 
-	        | @label, @count, @icon, @color 
-	        |
-	        */
 	        $this->index_statistic = array();
             $this->index_statistic[] = ['label'=>'Total SKUs','count'=>DB::table($this->table)->count('digits_code'),'icon'=>'fa fa-pie-chart','color'=>'blue','width'=>'col-sm-6'];
-
-
-
-	        /*
-	        | ---------------------------------------------------------------------- 
-	        | Add javascript at body 
-	        | ---------------------------------------------------------------------- 
-	        | javascript code in the variable 
-	        | $this->script_js = "function() { ... }";
-	        |
-	        */
-	        $this->script_js = NULL;
-
-
-            /*
-	        | ---------------------------------------------------------------------- 
-	        | Include HTML Code before index table 
-	        | ---------------------------------------------------------------------- 
-	        | html code to display it before index table
-	        | $this->pre_index_html = "<p>test</p>";
-	        |
-	        */
-	        $this->pre_index_html = null;
-	        
-	        
-	        
-	        /*
-	        | ---------------------------------------------------------------------- 
-	        | Include HTML Code after index table 
-	        | ---------------------------------------------------------------------- 
-	        | html code to display it after index table
-	        | $this->post_index_html = "<p>test</p>";
-	        |
-	        */
-	        $this->post_index_html = null;
-	        
-	        
-	        
-	        /*
-	        | ---------------------------------------------------------------------- 
-	        | Include Javascript File 
-	        | ---------------------------------------------------------------------- 
-	        | URL of your javascript each array 
-	        | $this->load_js[] = asset("myfile.js");
-	        |
-	        */
-	        $this->load_js = array();
-	        
-	        
-	        
-	        /*
-	        | ---------------------------------------------------------------------- 
-	        | Add css style at body 
-	        | ---------------------------------------------------------------------- 
-	        | css code in the variable 
-	        | $this->style_css = ".style{....}";
-	        |
-	        */
-	        $this->style_css = NULL;
-	        
-	        
-	        
-	        /*
-	        | ---------------------------------------------------------------------- 
-	        | Include css File 
-	        | ---------------------------------------------------------------------- 
-	        | URL of your css each array 
-	        | $this->load_css[] = asset("myfile.css");
-	        |
-	        */
-	        $this->load_css = array();
-	        
 	        
 	    }
 
-
-	    /*
-	    | ---------------------------------------------------------------------- 
-	    | Hook for button selected
-	    | ---------------------------------------------------------------------- 
-	    | @id_selected = the id selected
-	    | @button_name = the name of button
-	    |
-	    */
 	    public function actionButtonSelected($id_selected,$button_name) {
 	        //Your code here
             if($button_name == 'set_item_serialize') {
-            	DB::table('items')->whereIn('id',$id_selected)->update([ 'has_serial'=> 1 ]);
+            	Item::whereIn('id',$id_selected)->update([ 'has_serial'=> 1 ]);
             	
             }
             else if($button_name == 'set_item_general') {
-            	DB::table('items')->whereIn('id',$id_selected)->update([ 'has_serial'=> 0 ]);
+            	Item::whereIn('id',$id_selected)->update([ 'has_serial'=> 0 ]);
             	
             } 
 			else if($button_name == 'create_pos_item') {
-            	$items = DB::table('items')->whereIn('id',$id_selected)->get();
+            	$items = Item::whereIn('id',$id_selected)->get();
             	foreach ($items as $key => $value) {
 					(new POSPushController)->posCreateItem(
 						$value->digits_code,
@@ -277,104 +121,20 @@
 						$value->has_serial);
 				}
             } 
+			else if($button_name == 'sync_rtl_qty'){
+				$items = Item::whereIn('id',$id_selected)->get(['id','digits_code']);
+				foreach ($items as $key => $value) {
+					try {
+						$rtlQty = OnhandQty::getOnhand()->where('mtl_system_items_b.segment1',$value->digits_code)->first();
+						$item = Item::find($value->id);
+						$item->reserve_qty = $rtlQty->quantity;
+						$item->save();
+					} catch (\Exception $e) {
+						\Log::error('Error!'.$e->getMessage());
+					}
+				}
+			}
 			
 	    }
-
-
-	    /*
-	    | ---------------------------------------------------------------------- 
-	    | Hook for manipulate query of index result 
-	    | ---------------------------------------------------------------------- 
-	    | @query = current sql query 
-	    |
-	    */
-	    public function hook_query_index(&$query) {
-	        //Your code here
-	            
-	    }
-
-	    /*
-	    | ---------------------------------------------------------------------- 
-	    | Hook for manipulate row of index table html 
-	    | ---------------------------------------------------------------------- 
-	    |
-	    */    
-	    public function hook_row_index($column_index,&$column_value) {	        
-	    	//Your code here
-	    }
-
-	    /*
-	    | ---------------------------------------------------------------------- 
-	    | Hook for manipulate data input before add data is execute
-	    | ---------------------------------------------------------------------- 
-	    | @arr
-	    |
-	    */
-	    public function hook_before_add(&$postdata) {        
-	        //Your code here
-
-	    }
-
-	    /* 
-	    | ---------------------------------------------------------------------- 
-	    | Hook for execute command after add public static function called 
-	    | ---------------------------------------------------------------------- 
-	    | @id = last insert id
-	    | 
-	    */
-	    public function hook_after_add($id) {        
-	        //Your code here
-
-	    }
-
-	    /* 
-	    | ---------------------------------------------------------------------- 
-	    | Hook for manipulate data input before update data is execute
-	    | ---------------------------------------------------------------------- 
-	    | @postdata = input post data 
-	    | @id       = current id 
-	    | 
-	    */
-	    public function hook_before_edit(&$postdata,$id) {        
-	        //Your code here
-
-	    }
-
-	    /* 
-	    | ---------------------------------------------------------------------- 
-	    | Hook for execute command after edit public static function called
-	    | ----------------------------------------------------------------------     
-	    | @id       = current id 
-	    | 
-	    */
-	    public function hook_after_edit($id) {
-	        //Your code here 
-
-	    }
-
-	    /* 
-	    | ---------------------------------------------------------------------- 
-	    | Hook for execute command before delete public static function called
-	    | ----------------------------------------------------------------------     
-	    | @id       = current id 
-	    | 
-	    */
-	    public function hook_before_delete($id) {
-	        //Your code here
-
-	    }
-
-	    /* 
-	    | ---------------------------------------------------------------------- 
-	    | Hook for execute command after delete public static function called
-	    | ----------------------------------------------------------------------     
-	    | @id       = current id 
-	    | 
-	    */
-	    public function hook_after_delete($id) {
-	        //Your code here
-
-	    }
-
 
 	}
