40114676035424e04ff5e4c4de5151bd8b125b37
Mike Rodelas <mikerodelas@digits.ph>
2024-07-17 16:45:41 +0800

    adjust pullout pick confirm module - remove pos side during picking

--- app/Pullout.php
+++ app/Pullout.php
@@ -7,4 +7,34 @@ use Illuminate\Database\Eloquent\Model;
 class Pullout extends Model
 {
     protected $table = 'pullout';
+    protected $guarded = [];
+
+    public function scopeGetDetailWithMoReason($query, $stNumber) {
+        return $query->join('reason', 'pullout.reason_id', '=', 'reason.bea_mo_reason')
+            ->leftJoin('transport_types', 'pullout.transport_types_id', '=', 'transport_types.id')
+            ->where('st_document_number', $stNumber)
+            ->select('pullout.*','reason.pullout_reason','reason.id as reason_id','transport_types.transport_type');
+    }
+
+    public function scopeGetDetailWithSoReason($query, $stNumber) {
+        return $query->join('reason', 'pullout.reason_id', '=', 'reason.bea_so_reason')
+            ->leftJoin('transport_types', 'pullout.transport_types_id', '=', 'transport_types.id')
+            ->where('st_document_number', $stNumber)
+            ->select('pullout.*','reason.pullout_reason','reason.id as reason_id','transport_types.transport_type');
+    }
+
+    public function scopeGetItems($query, $stNumber) {
+        return $query->where('st_document_number',$stNumber)
+            ->select('id','item_code','quantity');
+    }
+
+    public function scopeGetItemQty($query, $stNumber) {
+        return $query->where('st_document_number', $stNumber)
+			->sum('quantity');
+    }
+
+    public function scopeGetByRef($query, $stNumber) {
+        return $query->where('st_document_number', $stNumber);
+    }
+
 }
--- app/StoreName.php
+++ app/StoreName.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class StoreName extends Model
 {
     protected $table = 'stores';
+    protected $guarded = [];
+
+    public function scopeGetStoreByName($query, $storeName){
+        return $query->where('pos_warehouse', $storeName);
+    }
+
 
     public function scopeGetMoCustomerName($query, $customer_name){
         return $query->where('bea_mo_store_name', $customer_name)->first();
@@ -14,5 +20,26 @@ class StoreName extends Model
 
     public function scopeGetSoCustomerName($query, $customer_name){
         return $query->where('bea_so_store_name', $customer_name)->first();
+    }
+
+    public function scopeGetInboundTripStores($query, $pullouts, $transfers){
+        return $query->whereIn('channel_id',[1,2])
+            ->where('status','ACTIVE')
+            ->where(function ($subquery) use ($pullouts, $transfers) {
+                $subquery->whereIn('id',array_column($pullouts, 'stores_id'))
+                ->orWhereIn('id',array_column($transfers, 'stores_id'));
+            })->select('id','pos_warehouse','pos_warehouse_transit','pos_warehouse_name')
+            ->orderBy('pos_warehouse_name','ASC');
+    }
+
+    public function scopeGetOutboundTripStores($query, $deliveries, $transfers){
+        return $query->whereIn('channel_id',[1,2])
+            ->where('status','ACTIVE')
+            ->where(function ($subquery) use ($deliveries, $transfers) {
+                $subquery->whereIn('id',array_column($deliveries, 'stores_id'))
+                ->orWhereIn('id',array_column($transfers, 'stores_id_destination'));
+            })->select('id','pos_warehouse','pos_warehouse_transit','pos_warehouse_name')
+            ->orderBy('pos_warehouse_name','ASC');
+        
     }
 }
