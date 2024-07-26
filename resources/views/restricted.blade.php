430072d688294d867c47d0a6f746d5956ac9a863
Mike Rodelas <mikerodelas@digits.ph>
2024-07-27 02:00:04 +0800

    update restriction message

--- resources/views/restricted.blade.php
+++ resources/views/restricted.blade.php
@@ -6,7 +6,7 @@
 @section('content')
 <div class="modal fade" id="restrictedModal" tabindex="-1" role="dialog" aria-labelledby="restrictedModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content panel-warning">
+        <div class="modal-content panel-danger">
             <div class="modal-header panel-heading">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
@@ -16,10 +16,10 @@
             <div class="modal-body">
                 <h3>You do not have permission to access this page at this time.</h3>
                 <br>
-                <h2 style="color: red;" class="text-center">MAGHINTAY KA!!!</h2>
+                <h2 style="color: red;" class="text-center">PLEASE WAIT UNTIL FURTHER NOTICE!</h2>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-secondary pull-left" data-dismiss="modal">Close</button>
                 <a href="{{ CRUDBooster::mainpath() }}" class="btn btn-primary">Back</a>
             </div>
         </div>
