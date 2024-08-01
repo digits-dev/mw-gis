69215dce7263fec97fd8faa2927ec11f02da69c9
Mike Rodelas <mikerodelas@digits.ph>
2024-08-01 10:58:44 +0800

    update footers for printing purposes

---
+++ resources/views/vendor/crudbooster/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer no-print">
+    <!-- To the right -->
+    <div class="pull-{{ trans('crudbooster.right') }} hidden-xs">
+        {{ trans('crudbooster.powered_by') }} Digits
+    </div>
+    <!-- Default to the left -->
+    <strong>{{ trans('crudbooster.copyright') }} &copy; <?php echo date('Y') ?>. {{ trans('crudbooster.all_rights_reserved') }}.</strong>
+</footer>
